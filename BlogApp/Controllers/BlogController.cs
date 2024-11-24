818c6b7c65f2d77ebcc0c6a6e2a6005fdbf0e667
Halit TİRYAKİ <halittiryaki61@outlook.com>
2024-11-24 19:57:23 +0300

    Add project files.

---
+++ BlogApp/Controllers/BlogController.cs
@@ -0,0 +1,49 @@
+﻿using BlogApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private static int _nextId = 1;
+        private static List<BlogPost> _posts = new List<BlogPost>();
+        public IActionResult Index()
+        {
+            ViewBag.TotalPosts =_posts.Count;
+            ViewData["PageTitle"] = "Blog Gönderileri";
+            return View(_posts);
+        }
+        public IActionResult Create()
+        {
+            ViewBag.CurrentTime = DateTime.Now;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BlogPost blogpost)
+        {
+            if (ModelState.IsValid)
+            {
+                blogpost.Id = _nextId++;
+                _posts.Add(blogpost);
+                TempData["SuccessMessage"] = "Blog gönderisi başarı ile oluşturuldu";
+
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.CurrentTime = DateTime.Now; 
+            return View(blogpost);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var posts = _posts.FirstOrDefault(x => x.Id == id);
+            if(posts == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["CreatedAgo"] = $"{(DateTime.Now - posts.CreateAt).TotalMinutes} dakika önce";
+            return View(posts);
+        }
+    }
+}
---
+++ BlogApp/Models/BlogPost.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Başlık gereklidir.")]
+        [StringLength(100, ErrorMessage = "100 Karakterden fazla olamaz.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "İçerik gereklidir.")]
+        public string Content { get; set; }
+
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+    }
+}
